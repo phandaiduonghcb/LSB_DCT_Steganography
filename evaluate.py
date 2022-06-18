ade7c9c5f037e2a41c9e9115c2e4ec01442692c4
phandaiduonghcb <phandaiduonghcb@gmail.com>
2022-06-18 21:33:18 +0700

    add paper dct

--- evaluate.py
+++ evaluate.py
@@ -6,7 +6,7 @@ from sewar import mse, ssim, psnr
 
 original_im_paths = glob.glob('original_images/*')
 tiff_png_paths = glob.glob('encoded_images/GIMP_tiff_to_png/*')
-lsb_paths = glob.glob('encoded_images/*.png')
+png_paths = glob.glob('encoded_images/*.png')
 
 for original_im_path in original_im_paths:
     origninal_im = plt.imread(original_im_path)*255
@@ -15,7 +15,7 @@ for original_im_path in original_im_paths:
     img_name = os.path.basename(original_im_path).split('.')[0]
 
     print('### '+original_im_path)
-    for encoded_path in tiff_png_paths + lsb_paths:
+    for encoded_path in tiff_png_paths + png_paths:
         if img_name in os.path.basename(encoded_path):
             encoded_im = plt.imread(encoded_path)*255
             encoded_im = encoded_im.astype(np.uint8)
